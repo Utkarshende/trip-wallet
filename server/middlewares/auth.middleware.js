63632b25bd0091fd8a3ba43311cd0a16e4198666
Utkarsha Shende <utkarshapd1403@gmail.com>
2026-04-15 15:02:18 +0530

    feat: add authentication middleware for protected routes

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,20 @@
+// middlewares/auth.middleware.js
+
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+
+  try {
+    token = token.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token failed" });
+  }
+};
