3530544b8647bc4d0ca7211effd9c87cc790a7f4
Utkarsha Shende <utkarshapd1403@gmail.com>
2026-04-15 15:04:36 +0530

    feat: setup express app and route integration

---
+++ server/app.js
@@ -0,0 +1,17 @@
+// app.js
+
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.routes.js";
+import groupRoutes from "./routes/group.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/group", groupRoutes);
+
+export default app;
